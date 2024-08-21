531c94902387cd4bdd4a2cf5953436196d0778e7
Kas1o <twins_t@outlook.com>
2024-08-21 08:33:54 +0800

    ShareGPT Dataset

---
+++ Util/Dataset/DatasetUtil.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace SharperLLM.Util.Dataset
+{
+    public static class DatasetUtil
+    {
+        public static List<ShareGPTDatasetTerm> LoadShareGPTDataset(string datasetContent)
+        {
+            try
+            {
+                var shareGPTDatasetEntries = JsonConvert.DeserializeObject<List<ShareGPTDatasetTerm>>(datasetContent);
+                return shareGPTDatasetEntries;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public static string SaveShareGPTDataset(List<ShareGPTDatasetTerm> dataset)
+        {
+            return JsonConvert.SerializeObject(dataset);
+        }
+    }
+}
---
+++ Util/Dataset/ShareGPTDataset.cs
@@ -0,0 +1,46 @@
+﻿namespace SharperLLM.Util.Dataset
+{
+    public class ShareGPTDatasetConversationTerm
+    {
+        public string from { get; set; }
+        public string value { get; set; }
+    }
+    public class ShareGPTDatasetTerm
+    {
+        public string system { get; set; }
+        public List<ShareGPTDatasetConversationTerm> conversations { get; set; } = new();
+
+        public void WriteToPromptBuilder(PromptBuilder originPromptBuilder)
+        {
+            originPromptBuilder.System = system;
+            originPromptBuilder.Messages = conversations.Select(selector: x => (x.value, x.from switch
+            {
+                "system" => PromptBuilder.From.system,
+                "assistant" => PromptBuilder.From.assistant,
+                "user" => PromptBuilder.From.user,
+                "gpt" => PromptBuilder.From.assistant,
+                "human" => PromptBuilder.From.user,
+                _ => throw new Exception(message: $"Unknown From: {x.from}")
+            })).ToArray();
+        }
+
+        public static ShareGPTDatasetTerm CreateFromPromptBuilder(PromptBuilder pb, string systemTag = "system", string assitantTag = "gpt", string userTag = "human")
+        {
+            var term = new ShareGPTDatasetTerm();
+            term.system = pb.System;
+            foreach (var item in pb.Messages)
+            {
+                term.conversations.Add(new ShareGPTDatasetConversationTerm{
+                    from = item.Item2 switch 
+                    {
+                        PromptBuilder.From.system => systemTag,
+                        PromptBuilder.From.assistant => assitantTag,
+                        PromptBuilder.From.user => userTag
+                    },
+                    value = item.Item1
+                });
+            }
+            return term;
+        }
+    }
+}
