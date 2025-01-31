7170ffe905b0e8f7c74c62cf4663f453d19f67a3
Kas1o <twins_t@outlook.com>
2025-01-31 12:15:54 +0800

    修改 koboldapi max length 默认为 8192

---
+++ API/ToolCallingForTextGenerationAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharperLLM.API
+{
+	public class ToolCallingForTextGenerationAPI(iLLMAPI _origin)
+	{
+		iLLMAPI origin = _origin;
+
+	}
+}
