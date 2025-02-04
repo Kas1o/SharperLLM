2791dcc07251d469a01d2ab50662a0546635723c
Kas1o <twins_t@outlook.com>
2025-02-04 18:55:20 +0800

    CustomPromptBuilder And CustomRole

---
+++ Util/CustomPromptBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharperLLM.Util
+{
+	public class CustomPromptBuilder
+	{
+		public List<(CustomRole cr, string content)> Messages = new();
+
+		string GenerateCleanPrompt()
+		{
+			var sb = new StringBuilder();
+			foreach (var message in Messages)
+			{
+				sb
+				.Append(message.cr.StartSequence)
+				.Append(message.content)
+				.Append(message.cr.EndSequence);
+			}
+			return sb.ToString();
+		}
+
+		string GeneratePromptWithGenerationPrefix(CustomRole generationRole)
+		{
+			return GenerateCleanPrompt() + generationRole.StartSequence;
+		}
+	}
+}
---
+++ Util/CustomRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharperLLM.Util
+{
+	public class CustomRole
+	{
+		public string StartSequence { get; set; }
+		public string EndSequence { get; set; }
+	}
+}
