1a424d4d6bb46ec39754b4f62e7e015292d0b169
Kas1o <twins_t@outlook.com>
2025-04-08 23:56:35 +0800

    responseex大改。

--- API/ResponseEx.cs
+++ API/ResponseEx.cs
@@ -9,7 +9,23 @@ namespace SharperLLM.API
 {
 	public class ResponseEx
 	{
-		public string content;
-		public List<Tool> toolCallings;
+		public required string content { get; set; }
+		public List<ToolCall>? toolCallings { get; set; }
+		public required FinishReason FinishReason { get; set; }
+	}
+
+	public enum FinishReason
+	{
+		Stop,
+		Length,
+		ContentFilter,
+		FunctionCall,
+	}
+
+	public class ToolCall
+	{
+		public required string name { get; set; }
+		public required string id { get; set; }
+		public string? arguments { get; set; }//in json object
 	}
 }
---
+++ Util/ToolChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharperLLM.Util
+{
+    struct ToolChatMessage 
+    {
+        public string id;
+        public string content;
+    }
+}
